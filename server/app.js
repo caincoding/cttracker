4a004133011412116b7388f55417c358878d0739
John Cain <cain2311@gmail.com>
2017-07-27 20:40:28 -0500

    spacing change and added config file for database

--- server/app.js
+++ server/app.js
@@ -3,17 +3,16 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+import config from './config/database'
 import routes from './routes/routes'
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/cttracker', (err) => {
+mongoose.connect(config.database, (err) => {
   if(err) {
     console.log('Connections fucked')
   } else {
     console.log('Connection to database was successful')
   }
 })
-
-
 
 const app = express()
 
